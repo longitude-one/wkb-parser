8af5454d31ab5833f7b8c74c3176e308374fcf8d
Di <depodimon@gmail.com>
2025-12-27 20:51:02 +0400

    Fix deprecated ord() for PHP 8.4+

--- lib/LongitudeOne/Geo/WKB/Reader.php
+++ lib/LongitudeOne/Geo/WKB/Reader.php
@@ -68,9 +68,8 @@ class Reader
         $this->position = 0;
         $this->previous = 0;
 
-        if (ord($input) < 32) {
+        if (ord($input[0]) < 32) {
             $this->input = $input;
-
             return;
         }
 
